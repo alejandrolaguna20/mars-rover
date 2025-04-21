69a6e659bc269e98ab3a9041952f059a33985aaf
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-04-21 16:15:45 +0200

    fix: redirection issue

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (common for cloud platforms like Railway).
+     *
+     * @var string|array
+     */
+    protected $proxies = '*';
+
+    /**
+     * Trust all forwarded headers.
+     *
+     * @var int
+     */
+    protected $headers =
+    Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
